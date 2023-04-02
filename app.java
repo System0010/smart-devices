fc9be72dff61dffe81ffe8c1991719238d84c0db
System0010 <emilio.arevalozuniga@gmail.com>
2023-04-02 13:49:44 +0200

    3er commit = using propertly .toString()

--- app.java
+++ app.java
@@ -2,15 +2,11 @@ public class app{
 
   public static void main(String[] args){
     SmartDevices smartPhone = new SmartPhone(true, "cameraFXF",45.2d,"s22","c1223-disp","litio-a++",3600d,"space-gray",110d,true,300330,"samsumg","android16");
- 
-    String description = smartPhone.toString();
-    System.out.println(description);
+    System.out.println(smartPhone);
 
     SmartDevices smartWatch = new SmartWatch(true, "b221", 4200d, "iwatch2", "disp455", "b221", 4200d, "blue-cream", 2.5d, false, 1093273, "Appple", "IOA3");
-
-    description = smartWatch.toString();
-    System.out.println("*****\n\n" + description);
-
+    System.out.println("-----------------------------------------------\n************************************************");
+    System.out.println(smartWatch);
 
   }
 
