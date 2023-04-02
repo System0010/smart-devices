74021ea1035f93816174414ba63b9c3da1015154
System0010 <emilio.arevalozuniga@gmail.com>
2023-04-02 13:51:57 +0200

    4o commit = format code

--- SmartWatch.java
+++ SmartWatch.java
@@ -1,25 +1,26 @@
-public class SmartWatch extends SmartDevices{
- boolean formCircle;
- String batteryType;
- double maxBatteryTime;
+public class SmartWatch extends SmartDevices {
+  boolean formCircle;
+  String batteryType;
+  double maxBatteryTime;
 
- public SmartWatch(){}
+  public SmartWatch() {
+  }
 
- public SmartWatch(boolean formCircle, String batteryType, double maxTimeBattery, String modDevice, String displayTipe, String batterytipe, double maxBatteryTime, String colorDv,
-    double displaySize, Boolean inStock, int udManufactured, String brandDv, String oSys){
-  super(modDevice, displayTipe, batterytipe, maxTimeBattery, colorDv, displaySize, inStock, udManufactured, brandDv, oSys);
-  this.formCircle = formCircle;
-  this.batteryType = batteryType;
-  this.maxBatteryTime = maxBatteryTime;
- }
+  public SmartWatch(boolean formCircle, String batteryType, double maxTimeBattery, String modDevice, String displayTipe,
+      String batterytipe, double maxBatteryTime, String colorDv,
+      double displaySize, Boolean inStock, int udManufactured, String brandDv, String oSys) {
+    super(modDevice, displayTipe, batterytipe, maxTimeBattery, colorDv, displaySize, inStock, udManufactured, brandDv,
+        oSys);
+    this.formCircle = formCircle;
+    this.batteryType = batteryType;
+    this.maxBatteryTime = maxBatteryTime;
+  }
 
-@Override
-public String toString() {
-  String sDv = super.toString();//sDv 
-  return "SmartWatch [formCircle=" + formCircle + ", batteryType=" + batteryType + ", maxTimeBattery=" + maxTimeBattery
-      + "]" + sDv;
-}
-
-
- 
+  @Override
+  public String toString() {
+    String sDv = super.toString();// sDv
+    return "SmartWatch [formCircle=" + formCircle + ", batteryType=" + batteryType + ", maxTimeBattery="
+        + maxTimeBattery
+        + "]" + sDv;
+  }
 }
--- app.java
+++ app.java
@@ -1,15 +1,14 @@
-public class app{
+public class app {
 
-  public static void main(String[] args){
-    SmartDevices smartPhone = new SmartPhone(true, "cameraFXF",45.2d,"s22","c1223-disp","litio-a++",3600d,"space-gray",110d,true,300330,"samsumg","android16");
+  public static void main(String[] args) {
+    SmartDevices smartPhone = new SmartPhone(true, "cameraFXF", 45.2d, "s22", "c1223-disp", "litio-a++", 3600d,
+        "space-gray", 110d, true, 300330, "samsumg", "android16");
     System.out.println(smartPhone);
 
-    SmartDevices smartWatch = new SmartWatch(true, "b221", 4200d, "iwatch2", "disp455", "b221", 4200d, "blue-cream", 2.5d, false, 1093273, "Appple", "IOA3");
-    System.out.println("-----------------------------------------------\n************************************************");
+    SmartDevices smartWatch = new SmartWatch(true, "b221", 4200d, "iwatch2", "disp455", "b221", 4200d, "blue-cream",
+        2.5d, false, 1093273, "Appple", "IOA3");
+    System.out
+        .println("-----------------------------------------------\n************************************************");
     System.out.println(smartWatch);
-
   }
-
-  
-
 }
